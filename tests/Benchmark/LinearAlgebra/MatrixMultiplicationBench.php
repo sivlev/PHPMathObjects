94fa4a78cee8afa9b339c0ee4a913df1c37472e7
Sergei Ivlev <s.ivlev.me@gmail.com>
2024-04-25 14:55:44 +0200

    Add matrix multiplication benchmark

---
+++ tests/Benchmark/LinearAlgebra/MatrixMultiplicationBench.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * PHPMathObjects Library
+ *
+ * @see https://github.com/sivlev/PHPMathObjects
+ *
+ * @author Sergei Ivlev <sergei.ivlev@chemie.uni-marburg.de>
+ * @copyright (c) 2024 Sergei Ivlev
+ * @license https://opensource.org/license/mit The MIT License
+ *
+ * @note This software is distributed "as is", with no warranty expressed or implied, and no guarantee for accuracy or applicability to any purpose. See the license text for details.
+ */
+
+declare(strict_types=1);
+
+namespace PHPMathObjects\Tests\Benchmark\LinearAlgebra;
+
+use PhpBench\Attributes\BeforeMethods;
+use PhpBench\Attributes\Groups;
+use PhpBench\Attributes\Iterations;
+use PhpBench\Attributes\Revs;
+use PHPMathObjects\Exception\MatrixException;
+use PHPMathObjects\LinearAlgebra\Matrix;
+
+/**
+ * Benchmark for matrix multiplication with multiply() and mMultiply() methods
+ */
+#[Groups(["Linear Algebra"])]
+#[BeforeMethods('setUp')]
+class MatrixMultiplicationBench
+{
+    /**
+     * @var Matrix A
+     */
+
+    protected Matrix $a;
+
+    /**
+     * @var Matrix B
+     */
+    protected Matrix $b;
+
+    /**
+     * @return void
+     * @throws MatrixException
+     */
+    public function setUp(): void
+    {
+        $this->a = Matrix::fill(100, 100, 5.5);
+        $this->b = Matrix::fill(100, 100, -16.77);
+    }
+
+    /**
+     * @return void
+     * @throws MatrixException
+     */
+    #[Iterations(5)]
+    #[Revs(10)]
+    public function benchMultiply(): void
+    {
+        $this->a->multiply($this->b);
+    }
+
+    /**
+     * @return void
+     * @throws MatrixException
+     */
+    #[Iterations(5)]
+    #[Revs(10)]
+    public function benchMMultiply(): void
+    {
+        $this->a->mMultiply($this->b);
+    }
+}
