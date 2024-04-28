698b0d997044fc01731cd9cd7d680bf20c0ccc07
sivlev <sivlev@users.noreply.github.com>
2024-04-28 00:33:02 +0000

    Deploying to docs from @ sivlev/phpmathobjects@7676d6a45e2a630b914e6a7c9bdefa0f7f9ce4c3 🚀

--- js/searchIndex.js
+++ js/searchIndex.js
@@ -51,6 +51,11 @@ Search.appendIndex(
             "summary": "Abstract\u0020method\u0020for\u0020class\u002Dspecific\u0020data\u0020validation",
             "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_validateDataClassSpecific"
         },                {
+            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003AclearCache\u0028\u0029",
+            "name": "clearCache",
+            "summary": "Abstract\u0020method\u0020for\u0020flushing\u0020cached\u0020properties,\u0020such\u0020as\u0020determinants\u0020etc.",
+            "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_clearCache"
+        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003AtoArray\u0028\u0029",
             "name": "toArray",
             "summary": "Returns\u0020the\u0020matrix\u0020as\u0020array",
@@ -86,11 +91,6 @@ Search.appendIndex(
             "summary": "Returns\u0020the\u0020value\u0020of\u0020a\u0020matrix\u0020element\u0020with\u0020given\u0020indices.",
             "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_get"
         },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003Aset\u0028\u0029",
-            "name": "set",
-            "summary": "Sets\u0020the\u0020matrix\u0020element\u0020with\u0020the\u0020given\u0020indices\u0020to\u0020the\u0020given\u0020value",
-            "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_set"
-        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003AoffsetExists\u0028\u0029",
             "name": "offsetExists",
             "summary": "OffsetExists\u0028\u0029\u0020method\u0020of\u0020the\u0020ArrayAccess\u0020interface",
@@ -101,25 +101,20 @@ Search.appendIndex(
             "summary": "OffsetGet\u0028\u0029\u0020method\u0020of\u0020the\u0020ArrayAccess\u0020interface",
             "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_offsetGet"
         },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003AoffsetSet\u0028\u0029",
-            "name": "offsetSet",
-            "summary": "OffsetSet\u0028\u0029\u0020method\u0020of\u0020the\u0020ArrayAccess\u0020interface",
-            "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_offsetSet"
-        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003AoffsetUnset\u0028\u0029",
             "name": "offsetUnset",
             "summary": "Unset\u0028\u0029\u0020method\u0020of\u0020the\u0020ArrayAccess\u0020interface\u0020\u0028not\u0020implemented\u0029",
             "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_offsetUnset"
         },                {
+            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003AisSquare\u0028\u0029",
+            "name": "isSquare",
+            "summary": "Returns\u0020true\u0020if\u0020the\u0020matrix\u0020is\u0020square\u0020\u0028i.e.\u0020number\u0020of\u0020rows\u0020is\u0020equal\u0020to\u0020number\u0020of\u0020columns\u0029",
+            "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_isSquare"
+        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003Atranspose\u0028\u0029",
             "name": "transpose",
             "summary": "Matrix\u0020transpose",
             "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_transpose"
-        },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003AmTranspose\u0028\u0029",
-            "name": "mTranspose",
-            "summary": "Mutating\u0020matrix\u0020transpose\u0020\u0028the\u0020current\u0020matrix\u0020will\u0020be\u0020modified\u0029",
-            "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#method_mTranspose"
         },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003A__toString\u0028\u0029",
             "name": "__toString",
@@ -136,6 +131,11 @@ Search.appendIndex(
             "summary": "Two\u002Ddimensional\u0020array\u0020to\u0020store\u0020the\u0020matrix\u0020data",
             "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#property_matrix"
         },                {
+            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\AbstractMatrix\u003A\u003A\u0024isCachePresent",
+            "name": "isCachePresent",
+            "summary": "If\u0020true,\u0020then\u0020at\u0020least\u0020one\u0020cacheable\u0020property\u0020is\u0020cached,\u0020and\u0020the\u0020cache\u0020must\u0020be\u0020cleared\u0020by\u0020any\u0020mutation\u0020of\u0020the\u0020matrix",
+            "url": "classes/PHPMathObjects-LinearAlgebra-AbstractMatrix.html#property_isCachePresent"
+        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix",
             "name": "Matrix",
             "summary": "Implementation\u0020of\u0020the\u0020AbstractMatrix\u0020class\u0020to\u0020manipulate\u0020numeric\u0020matrices",
@@ -151,55 +151,35 @@ Search.appendIndex(
             "summary": "Implementation\u0020of\u0020the\u0020abstract\u0020class\u002Dspecific\u0020data\u0020validation\u0020method\u0020for\u0020numeric\u0020matrices",
             "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_validateDataClassSpecific"
         },                {
+            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AclearCache\u0028\u0029",
+            "name": "clearCache",
+            "summary": "Implementation\u0020of\u0020abstract\u0020clearCache\u0028\u0029\u0020method\u0020for\u0020numeric\u0020matrices",
+            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_clearCache"
+        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003Aadd\u0028\u0029",
             "name": "add",
             "summary": "Matrix\u0020addition",
             "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_add"
-        },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AmAdd\u0028\u0029",
-            "name": "mAdd",
-            "summary": "Mutating\u0020matrix\u0020addition\u0020\u0028the\u0020result\u0020is\u0020stored\u0020in\u0020the\u0020current\u0020matrix\u0029",
-            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_mAdd"
         },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003Asubtract\u0028\u0029",
             "name": "subtract",
             "summary": "Matrix\u0020subtraction",
             "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_subtract"
         },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AmSubtract\u0028\u0029",
-            "name": "mSubtract",
-            "summary": "Mutating\u0020matrix\u0020subtraction\u0020\u0028the\u0020result\u0020is\u0020stored\u0020in\u0020the\u0020current\u0020matrix\u0029",
-            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_mSubtract"
-        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003Amultiply\u0028\u0029",
             "name": "multiply",
             "summary": "Matrix\u0020multiplication",
             "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_multiply"
-        },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AmMultiply\u0028\u0029",
-            "name": "mMultiply",
-            "summary": "Mutating\u0020matrix\u0020multiplication\u0020\u0028the\u0020result\u0020is\u0020stored\u0020in\u0020the\u0020current\u0020matrix\u0029",
-            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_mMultiply"
         },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AmultiplyByScalar\u0028\u0029",
             "name": "multiplyByScalar",
             "summary": "Multiplication\u0020of\u0020a\u0020matrix\u0020by\u0020a\u0020scalar\u0020elementwise",
             "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_multiplyByScalar"
         },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AmMultiplyByScalar\u0028\u0029",
-            "name": "mMultiplyByScalar",
-            "summary": "Mutating\u0020multiplication\u0020of\u0020a\u0020matrix\u0020by\u0020a\u0020scalar\u0020elementwise\u0020\u0028result\u0020stored\u0020in\u0020the\u0020current\u0020matrix\u0029",
-            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_mMultiplyByScalar"
-        },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AchangeSign\u0028\u0029",
             "name": "changeSign",
             "summary": "Change\u0020of\u0020signs\u0020of\u0020all\u0020elements",
             "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_changeSign"
-        },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AmChangeSign\u0028\u0029",
-            "name": "mChangeSign",
-            "summary": "Mutating\u0020change\u0020of\u0020signs\u0020of\u0020all\u0020elements\u0020\u0028result\u0020stored\u0020in\u0020the\u0020current\u0020matrix\u0029",
-            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_mChangeSign"
         },                {
             "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003AisEqual\u0028\u0029",
             "name": "isEqual",
@@ -216,10 +196,10 @@ Search.appendIndex(
             "summary": "Calculation\u0020of\u0020the\u0020trace\u0020of\u0020a\u0020matrix\u0020\u0028defined\u0020for\u0020square\u0020matrices\u0020only\u0029",
             "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#method_trace"
         },                {
-            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003A\u0024traceCache",
-            "name": "traceCache",
+            "fqsen": "\\PHPMathObjects\\LinearAlgebra\\Matrix\u003A\u003A\u0024cacheTrace",
+            "name": "cacheTrace",
             "summary": "Cached\u0020value\u0020of\u0020the\u0020trace\u0020of\u0020a\u0020matrix",
-            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#property_traceCache"
+            "url": "classes/PHPMathObjects-LinearAlgebra-Matrix.html#property_cacheTrace"
         },                {
             "fqsen": "\\",
             "name": "\\",
